60eaa3b5384fe1ed21041644ed249d8c0cf005fd
xurde <uo271033@uniovi.es>
2021-04-28 11:02:33 +0200

    Registro usuarios

---
+++ app.js
@@ -0,0 +1,31 @@
+// Módulos
+let express = require('express');
+let app = express();
+
+let crypto = require('crypto');
+
+let mongo = require('mongodb');
+let swig = require('swig');
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+app.use(express.static('public'));
+
+// Variables
+app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@wallapop-shard-00-00.msduo.mongodb.net:27017,wallapop-shard-00-01.msduo.mongodb.net:27017,wallapop-shard-00-02.msduo.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-otiliw-shard-0&authSource=admin&retryWrites=true&w=majority');
+
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
+
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+})
