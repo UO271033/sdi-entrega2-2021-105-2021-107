69452793f6914a7dd206122cb068b689b22f6d4d
UO265181 <UO265181@uniovi.es>
2021-05-11 18:26:56 +0200

    Arreglos W7

--- routes/rofertas.js
+++ routes/rofertas.js
@@ -47,6 +47,7 @@ module.exports = function(app, swig, gestorBD, validator, logger) {
     });
 
     app.get('/ofertas/propias', function (req, res) {
+        logger.debug("GET/ofertas/propias");
         let criterio = {"usuario" : req.session.usuario.email};
 
         gestorBD.obtenerOfertasUsuario(criterio, function (ofertas) {
